ae683825dac6bf72293e3aa47b24558d9354c161
josedrios <joserios0531@gmail.com>
2025-12-21 17:42:28 -0800

    Fixed pagniation error (was not awaiting for params, where it gives me the current page number for blogs)

--- src/app/blog/pages/[page]/page.jsx
+++ src/app/blog/pages/[page]/page.jsx
@@ -1,17 +1,23 @@
 import { getPaginatedPosts } from '@/lib/blog';
 import BlogList from '@/components/BlogList';
-import BlogAside from '@/components/BlogAside'
+import BlogAside from '@/components/BlogAside';
 
 export default async function BlogPage({ params }) {
-    const page = parseInt(params.page, 10);
+    const { page } = await params;
+    const pageNumber = parseInt(page, 10);
     const { posts, totalPages } = await getPaginatedPosts(page, 5);
 
     return (
         <main id="blog-page">
             <BlogAside />
             <div className="content">
-                <BlogList posts={posts} currentPage={page} totalPages={totalPages} />
+                <BlogList
+                    posts={posts}
+                    currentPage={pageNumber}
+                    totalPages={totalPages}
+                />
             </div>
         </main>
     );
 }
+
