5d83b6b99d1681c7d134cee565c8ea59b7d8cc3d
Jose Rios <joserios0531@gmail.com>
2026-04-19 11:54:08 -0700

    Small tweaks, retitled some blogs to have the brain dump prefix, added danger variant for note component

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,6 +2,7 @@ import { getSortedPostsData } from "@/lib/blogs";
 import BlogCard from "@/components/BlogCard";
 import Header from "@/components/Header";
 import { Metadata } from "next";
+import Note from "@/components/Note";
 
 export const metadata: Metadata = {
   title: "Blog",
@@ -13,6 +14,10 @@ export default function Blog() {
   return (
     <main>
       <Header type={1}>My Blog</Header>
+      <Note title="disclaimer">
+        This is a very unserious, all over the place, typo infested blog page. I
+        just want to be able to write better and share my thoughts.
+      </Note>
       <div className="blog-card__container">
         {blogs.map((blog) => (
           <BlogCard
--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,12 +1,14 @@
 export default function Note({
   children,
   title = "note",
+  danger = false,
 }: {
   children: React.ReactNode;
   title?: string;
+  danger?: boolean;
 }) {
   return (
-    <div className="note">
+    <div className={`note ${danger && "note--danger"}`}>
       <p className="note__text">
         <span className="note__prefix">{title.toUpperCase()}:</span>
         {children}
