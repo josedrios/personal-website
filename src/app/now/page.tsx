44ca05e66cffbb581e73a7f2f46d7790e1c52f02
Jose Rios <joserios0531@gmail.com>
2026-06-26 09:35:18 -0700

    Proof read to Neuron Replenishment blog and updated Now page

--- src/app/now/page.tsx
+++ src/app/now/page.tsx
@@ -17,32 +17,33 @@ export default function Now() {
         alt="A picture of Tony Stark working on his first iteration of his Iron Man suit. This is meant to be funny because I am using this image to represent what I am hypothetically, possibly doing right now."
         note="For the record, this image is not of me. It is of movie star Robert Downey Jr."
       />
-      <p>
-        I currently just finished reading a python textbook to spruce up my
-        python. Why? Because I want to learn data structures and algorithms.
-        Why? Certainly not because it will bring up my chances of getting hired
-        as a software engineer in this time of AI. Those chances are already
-        close to 0 if not 0. It is because I want to become a better programmer.
-      </p>
-      <p>
-        My urge to code and learn more on how to become a better programmer has
-        significantly risen because of this shift of focus. I am satisfied right
-        now with my current jobs and how life is going. My goal is not to become
-        a man in the field but a man who can build. This sounds like a whole
-        bunch of copium but I truly feel this way right now. The more I learn
-        the more I see how much code is just a huge sandbox. Maybe in the future
-        I will get a job in the field, who knows. But right now, I want to
-        become someone with great coding capabilities. Maybe build something
-        society can use.
-      </p>
-      <p>
-        <b>Summary:</b> Going to learn DSA and learn other general coding stuff.
-        Make stuff too. Maybe drink water and sleep here and there. If you are
-        reading this take some time to dissect what you are doing at this time
-        with your own life and what you would want to be doing. A little
-        funtivity.
-      </p>
-      <Note>Updated last on May 7, 2026</Note>
+      <Content />
+      <Note>Updated last on June 26, 2026</Note>
     </main>
+  );
+}
+
+function Content() {
+  return (
+    <>
+      <p>
+        Coding. Trying to become a better programmer. Websites. Polishing my
+        design token system to have a stronger foundation.{" "}
+      </p>
+      <p>
+        Soccer juggling. I&apos;ve been getting good, I mean I have always been
+        good but I&apos;ve been sprucing it up with more tricks.
+      </p>
+      <p>
+        Gaming. Pretty rage inducing. Movies, lots of movies. La La Land might
+        be the greatest movie of all time. Still needs to marinate in my head
+        though.
+      </p>
+      <p>
+        Perfecting my training. New split. Bench is increasing. Gains are
+        looking amazing. Still, more work can be done.
+      </p>
+      <p>Spending time with my amazing family and friends.</p>
+    </>
   );
 }
